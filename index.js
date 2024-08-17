bd7de984fbbb66bd4aeeb75a77a9c6cf28d05452
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-08-17 09:45:15 +0600

    bajar-tech server create

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req,res)=>{
+    res.send('bajar-tech sever is running ....');
+})
+
+app.listen(port, ()=> {
+    console.log(`app listening on port ${port}`);
+})
